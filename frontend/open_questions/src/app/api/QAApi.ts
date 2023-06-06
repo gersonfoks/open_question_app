15099015e3fb08888d39845b2ade08ac98fd7058
gerson <gerson.foks@gmail.com>
2023-06-06 16:31:41 +0200

    Adds basic qa form for prototyping

---
+++ frontend/open_questions/src/app/api/QAApi.ts
@@ -0,0 +1,7 @@
+
+export class QAApi{
+
+    gradeAnswer(question, answer) {
+        return 1.0
+    }
+}
---
+++ frontend/open_questions/src/app/contexts/context.tsx
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+import {QAApi} from "@/app/api/QAApi";
+
+
+
+export const qaApiContext: React.Context<any> = createContext(null);
